6c189333cc3849b4bb494a501bc9b47df6199b35
Chris Hendricks <crshenx@gmail.com>
2022-12-29 18:34:42 -0800

    Init nav

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './Landing.css'
+import NavBar from './NavBar'
 
 function Landing() {
   return (
     <div className='landing'>
         <div className='landing__title'>+2 Creative</div>
         <div className='landing__subtitle'>by Alisha Hendricks</div>
-
+        <NavBar />
     </div>
   )
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div className='nav'>
+        <div className='nav__bridal-concierge'>Bridal Concierge</div>
+    </div>
+  )
+}
+
+export default NavBar
