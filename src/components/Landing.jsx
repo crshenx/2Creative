ae0c0380843c9b188b861bc3a6d81310fb62b3ed
Chris Hendricks <crshenx@gmail.com>
2022-12-29 18:22:10 -0800

    Init react with vite

---
+++ src/components/Landing.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Landing.css'
+
+function Landing() {
+  return (
+    <div className='landing'>
+        <div className='landing__title'>+2 Creative</div>
+        <div className='landing__subtitle'>by Alisha Hendricks</div>
+
+    </div>
+  )
+}
+
+export default Landing
