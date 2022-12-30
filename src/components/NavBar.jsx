2b95058972f1cf6355be50e9a0a9c0a3a61b76c6
Chris Hendricks <crshenx@gmail.com>
2022-12-30 15:08:33 -0800

    mobile nav work

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,49 +1,59 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function NavBar() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const services = [
+    "Bridal Concierge",
+    "Event Planning",
+    "A la Carte",
+    "Testimonials",
+    "A Spontaneous Rythm",
+    "Contact",
+  ];
+  const divider = "✧";
 
-    const services = ['Bridal Concierge', 'Event Planning', 'A la Carte', 'Testimonials', 'A Spontaneous Rythm','Contact',]
-    const divider = '✧'
-    
-    let navItems = services.map((service,i) => {
-        if(i === services.length -1){
-            return (
-                <div className={`nav__item-parent-last`} key={i}>
-                <div className={`nav__item-last`}>{service}</div>
-                </div>
-            )
-        }
-        return (
-            <div className={`nav__item-parent`} key={i}>
-            <div className={`nav__item`}>{service}</div>
-            <div className={`nav__divider`}>{divider}</div>
-            </div>
-        )
-    })
+  let navItems = services.map((service, i) => {
+    if (i === services.length - 1) {
+      return (
+        <div className={`nav__item-parent-last`} key={i}>
+          <div className={`nav__item-last`}>{service}</div>
+        </div>
+      );
+    }
+    return (
+      <div className={`nav__item-parent`} key={i}>
+        <div className={`nav__item`}>{service}</div>
+        <div className={`nav__divider`}>{divider}</div>
+      </div>
+    );
+  });
 
-    let mobileNavItems = services.map((service,i) => {
-        return (
-            <div className={`nav__mobile-item`} key={i}>{service}</div>
-        )
-    })
+  let mobileNavItems = services.map((service, i) => {
+    return (
+      <div className={`nav__mobile-item`} key={i}>
+        {service}
+      </div>
+    );
+  });
 
-
+  function openMobileMenu() {
+    setMobileMenuOpen(!mobileMenuOpen);
+    console.log(mobileMenuOpen);
+  }
 
   return (
     <div>
-        <div className='nav'>{navItems}</div>
-        <div className='nav__mobile-expand'>
-            <div className='nav__mobile'>{mobileNavItems}</div>
-            </div>
+      <div className="nav__mobile-expand-button" onClick={openMobileMenu}>
+        ☰
+      </div>
+      <div className="nav">{navItems}</div>
+      <div
+        className={mobileMenuOpen ? "nav__mobile-open" : "nav__mobile-closed"}
+      >
+        {mobileNavItems}
+      </div>
     </div>
-  )
+  );
 }
 
-export default NavBar
-/**
- * 
- * <div className={`nav__item-parent-${service}`} key={i}>
-<div className={`nav__item-${service}`}>{service}</div>
-<div className={`nav__divider-${service}`}>{divider}</div>
-</div>
- */
+export default NavBar;
