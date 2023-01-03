9c4c50b42ae97e935d208b424dedeb7576c100a5
Chris Hendricks <crshenx@gmail.com>
2023-01-03 15:24:57 -0800

    code for socials added but were includint in footer

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
-import './Landing.css'
-import NavBar from './NavBar'
+import React from "react";
+import "./Landing.css";
+import NavBar from "./NavBar";
+// import Socials from "./Socials";
 
 function Landing() {
   return (
-    <div className='landing'>
-        <div className='landing__title'>+2 Creative</div>
-        <div className='landing__subtitle'>by Alisha Hendricks</div>
-        <NavBar />
+    <div className="landing">
+      <div className="landing__title-parent">
+        <div className="landing__title">+2 Creative</div>
+        <div className="landing__subtitle">by Alisha Hendricks</div>
+      </div>
+      {/* <Socials /> */}
+      <NavBar />
+      <hr className="title__divider"></hr>
     </div>
-  )
+  );
 }
 
-export default Landing
+export default Landing;
