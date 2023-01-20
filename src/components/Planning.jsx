7ad62de93adfa5a6da16ed114efff238ff9083a1
Chris Hendricks <crshenx@gmail.com>
2023-01-19 20:15:39 -0800

    planning mobile buttons init

--- src/components/Planning.jsx
+++ src/components/Planning.jsx
@@ -3,6 +3,9 @@ import PlanningBanner from "../assets/PlanningBanner.jpg";
 import "../css/Planning.css";
 
 function Planning() {
+  function buttonClick(e) {
+    console.log(e.target.className);
+  }
   return (
     <div className="planning">
       <img src={PlanningBanner} className="planning-banner" />
@@ -11,11 +14,42 @@ function Planning() {
         <div className="planning-title-planning">Planning</div>
       </div>
       <div className="planning-content-container">
-        <p>
+        <div className="planning-button-container">
+          <div className="btn welcome" onClick={buttonClick}>
+            <span>Welcome</span>
+          </div>
+
+          <div className="btn a-la" onClick={buttonClick}>
+            <span>A La Carte</span>
+          </div>
+          <div className="btn month-of" onClick={buttonClick}>
+            <span>Month of</span>
+          </div>
+          <div className="btn day-of" onClick={buttonClick}>
+            <span>Full Service</span>
+          </div>
+        </div>
+        <p className="planning__content-welcome">
           It would be my honor to collaborate with you to make your dream day a
           reality! I offer different levels and options of my planning guidance
           to allow you to choose what best fits your needs. Take a look at the
           brief descriptions below and choose what's right for you!{" "}
+        </p>
+        <p className="planning__content-carte">
+          A La Carte services allow you to pick and choose where you need bursts
+          of assistance from a professional. Each service is priced
+          individually.PERFECT for mini events as well!
+        </p>
+        <p className="planning__content-month">
+          Month OF" Coordination is best for the couple who handled it all and
+          now just wants to sit back and enjoy the ride. I run the show the
+          final month before the event all the way through the end of the big
+          day!{" "}
+        </p>
+        <p className="planning__content-full-service">
+          With full service planning, I will help you with everything from the
+          wording on your invitations and full event design, to our final
+          goodbye at the end of your best day ever!{" "}
         </p>
       </div>
     </div>
