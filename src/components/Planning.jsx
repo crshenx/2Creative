8559bed4123974618d22c9e2576112bb2b3a4644
Chris Hendricks <crshenx@gmail.com>
2023-02-08 11:04:32 -0800

    last branch commit before merge

--- src/components/Planning.jsx
+++ src/components/Planning.jsx
@@ -1,110 +1,99 @@
 import React, { useState } from "react";
 import PlanningBanner from "../assets/PlanningBanner.jpg";
 import "../css/Planning.css";
+import PlanningButtons from "./PlanningButtons";
+
+const planningContent = [
+  {
+    title: `Welcome`,
+    class: `welcome`,
+    text: `It would be my honor to collaborate with you to make your dream
+day a reality! I offer different levels and options of my
+planning guidance to allow you to choose what best fits your
+needs. Take a look at the brief descriptions below and choose
+what's right for you!`,
+  },
+  {
+    title: `A La Carte`,
+    class: `carte`,
+    text: ` A La Carte services allow you to pick and choose where you need
+bursts of assistance from a professional. Each service is priced
+individually.PERFECT for mini events as well!`,
+  },
+  {
+    title: `Month of`,
+    class: `month`,
+    text: `  Month OF" Coordination is best for the couple who handled it all
+and now just wants to sit back and enjoy the ride. I run the show
+the final month before the event all the way through the end of
+the big day!`,
+  },
+  {
+    title: `Full Service`,
+    class: `service`,
+    text: `With full service planning, I will help you with everything from
+the wording on your invitations and full event design, to our
+final goodbye at the end of your best day ever!`,
+  },
+];
 
 function Planning() {
-  const [activeButton1, setActiveButton1] = useState(true);
-  const [activeButton2, setActiveButton2] = useState(false);
-  const [activeButton3, setActiveButton3] = useState(false);
-  const [activeButton4, setActiveButton4] = useState(false);
+  const [buttonActive, setButtonActive] = useState(0);
+
+  const planningList = planningContent.map((plan, i) => {
+    return (
+      <div
+        key={i}
+        className={
+          buttonActive === i
+            ? ` active ${plan.class}`
+            : `inactive ${plan.class}`
+        }
+      >
+        <div
+          className={`text-title planning-content__text-title-${plan.class}`}
+        >
+          {plan.title}
+        </div>
+        <div className={`${plan.class}-text plannin__text`}>{plan.text}</div>
+      </div>
+    );
+  });
+
+  function buttonClick(i) {
+    setButtonActive(i);
+  }
+
+  console.log(buttonActive);
+
+  const buttonList = planningContent.map((plan, i) => {
+    return (
+      <div key={i} className={`btn ${i}`} onClick={() => buttonClick(i)}>
+        <span>{plan.title}</span>
+      </div>
+    );
+  });
 
   return (
     <div className="planning">
-      <div className="planning-title-container">
-        <div className="planning-title-event">event</div>
-        <div className="planning-title-planning">planning</div>
+      <div className="planning__divider-quote">
+        It would be my honor to collaborate with you to make your dream day a
+        reality! Take a look at the brief descriptions below and choose what's
+        right for you!
       </div>
-      <div className="planning_banner-container">
-        <img src={PlanningBanner} className="planning-banner" />
-      </div>
-      <div className="planning-content-container">
-        <div className="planning-button-container">
-          <div
-            className="btn welcome-btn"
-            onClick={() => {
-              if (!activeButton1) setActiveButton1(true);
-              setActiveButton2(false);
-              setActiveButton3(false);
-              setActiveButton4(false);
-            }}
-          >
-            <span>Welcome</span>
+      <div className="planning__pic-text-wrapper">
+        <div className="planning__pic-title">
+          <div className="planning-title-container">
+            <div className="planning-title-event">event</div>
+            <div className="planning-title-planning">planning</div>
           </div>
-
-          <div
-            className="btn a-la"
-            onClick={() => {
-              if (!activeButton2) setActiveButton2(true);
-              setActiveButton1(false);
-              setActiveButton3(false);
-              setActiveButton4(false);
-            }}
-          >
-            <span>A La Carte</span>
-          </div>
-          <div
-            className="btn month-of"
-            onClick={() => {
-              if (!activeButton3) setActiveButton3(true);
-              setActiveButton1(false);
-              setActiveButton2(false);
-              setActiveButton4(false);
-            }}
-          >
-            <span>Month of</span>
-          </div>
-          <div
-            className="btn full-service"
-            onClick={() => {
-              if (!activeButton4) setActiveButton4(true);
-              setActiveButton1(false);
-              setActiveButton2(false);
-              setActiveButton3(false);
-            }}
-          >
-            <span>Full Service</span>
+          <div className="planning_banner-container">
+            <img src={PlanningBanner} className="planning-banner" />
           </div>
         </div>
-        <div className="planning-content__text-container">
-          <div
-            className={activeButton1 ? "active welcome" : "inactive welcome"}
-          >
-            <div className="text-title planning-content__text-title-welcome">
-              Welcome
-            </div>
-            <div className="welcome-text">
-              It would be my honor to collaborate with you to make your dream
-              day a reality! I offer different levels and options of my planning
-              guidance to allow you to choose what best fits your needs. Take a
-              look at the brief descriptions below and choose what's right for
-              you!
-            </div>
-          </div>
-          <div className={activeButton2 ? "active carte" : "inactive carte"}>
-            <div className="text-title planning-content__text-title-carte">
-              A La Carte
-            </div>
-            A La Carte services allow you to pick and choose where you need
-            bursts of assistance from a professional. Each service is priced
-            individually.PERFECT for mini events as well!
-          </div>
-          <div className={activeButton3 ? "active month" : "inactive month"}>
-            <div className="text-title planning-content__text-title-month">
-              Month of
-            </div>
-            Month OF" Coordination is best for the couple who handled it all and
-            now just wants to sit back and enjoy the ride. I run the show the
-            final month before the event all the way through the end of the big
-            day!{" "}
-          </div>
-          <div className={activeButton4 ? "active full" : "inactive full"}>
-            <div className="text-title planning-content__text-title-service">
-              Full Service
-            </div>
-            With full service planning, I will help you with everything from the
-            wording on your invitations and full event design, to our final
-            goodbye at the end of your best day ever!{" "}
-          </div>
+        <div className="planning-content-container">
+          <PlanningButtons buttonList={buttonList} />
+          <div className="planning-content__text-container">{planningList}</div>
         </div>
       </div>
     </div>
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,26 +1,52 @@
 import React from "react";
 import "../css/Testimonials.css";
 
+const testimonials = [
+  {
+    text: `Alisha always made time to help us troubleshoot challenges along the
+way. Her positive energy and spirit helped us focus on what was
+important. Alisha communicated with our venue, and advocated for
+what we wanted in our wedding. She was incredibly thorough and
+thoughtful in helping us shape and make our dream wedding become a
+reality!`,
+    footer: `Lauren Q. // Full Service Planning 2021`,
+  },
+  {
+    text: `We absolutely recommend Alisha and her services; we could not have
+successfully held our wedding without her. Alisha is patient,
+attentive, and proactive. She is capable of taking the lead on
+projects and planning, and served as an instrumental point of
+contact for our venue and for our vendors. More importantly, Alisha
+spent time to connect with us and our families, and prioritized what
+was important to us. She was there to help with every challenge that
+we encountered and we are grateful to have worked with her.`,
+    footer: `Casey G. // Full Service Planning 2021`,
+  },
+  {
+    text: ` It was so great meeting and working with you! Your services are SO
+needed. Going forward with brides I meet at previews I’m 100% going
+to tell them about you. Everyone needs you, the vendors and the
+clients. You were so professional and kind and patient and what a
+dream support. I’ll share with my clients!`,
+    footer: `Tanya // CEO Page Beauty // Bridal Concierge Co-Vendor`,
+  },
+];
+
 function Testimonials() {
+  const testimonialList = testimonials.map((testi, i) => {
+    return (
+      <div key={i} className={`testimonial__wrapper testimonial__${i}`}>
+        <div className="testimonial__text">{testi.text}</div>
+        <div className="testimonial__footer">
+          <div className="testimonial__footer-client">{testi.footer}</div>
+        </div>
+      </div>
+    );
+  });
   return (
     <div className="testimonial">
       <div className="testimonial__title">testimonials</div>
-      <div className="testimonial__container">
-        {/* <div className="testimonial__opening-qotation qotation">"</div> */}
-        <div className="testimonial__text">
-          Alisha always made time to help us troubleshoot challenges along the
-          way. Her positive energy and spirit helped us focus on what was
-          important. Alisha communicated with our venue, and advocated for what
-          we wanted in our wedding. She was incredibly thorough and thoughtful
-          in helping us shape and make our dream wedding become a reality!
-        </div>
-        <div className="testimonial__footer">
-          <div className="testimonial__footer-client">
-            Lauren Q. // Full Service Planning 2021
-          </div>
-        </div>
-        {/* <div className="testimonial__closing-qotation qotation">"</div> */}
-      </div>
+      <div className="testimonial__container">{testimonialList}</div>
     </div>
   );
 }
