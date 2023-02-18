1d1948b78658efab6f9ff5e6cc986a1a5e43330e
Chris Hendricks <crshenx@gmail.com>
2023-02-17 19:38:48 -0800

    fixed mobile section in planning

--- src/components/PlanningButtons.jsx
+++ src/components/PlanningButtons.jsx
@@ -1,9 +1,5 @@
 import React from "react";
 
 export default function PlanningButtons({ buttonList }) {
-  return (
-    <div>
-      <div className="planning-button-container">{buttonList}</div>
-    </div>
-  );
+  return <div className="planning-button-container">{buttonList}</div>;
 }
